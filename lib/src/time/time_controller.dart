e6a1104be6b1173f3b7f7f65a4a447d69d0a90b5
Mike Fairhurst <michaelrfairhurst@gmail.com>
2024-06-27 14:50:27 -0700

    Add doc comments to TimeController

--- lib/src/time/time_controller.dart
+++ lib/src/time/time_controller.dart
@@ -1,11 +1,22 @@
 import 'package:dartnad0/src/time/time_control.dart';
 
+/// Class for creating move timers and/or killing engines that go over allotted
+/// move times.
 class TimeController {
+  /// Whether to use absolute or relative move timers.
+  ///
+  /// Relative timers will allow an engine to determine when the move timer
+  /// starts. This allows, for instance, http overhead to be "free." Absolute
+  /// timers use a fixed end time. So http overhead will reduce search time, and
+  /// clock differences may come into play as well.
   final bool absolute;
+
+  /// How much time is allotted for each move.
   final Duration moveTime;
 
   const TimeController(this.moveTime, {this.absolute = true});
 
+  /// Create a [TimeControl] for timing a single move.
   TimeControl makeMoveTimer() {
     if (absolute) {
       return AbsoluteTimeControl(DateTime.now().add(moveTime));
